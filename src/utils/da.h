79888fc948e9d155cdd660536103c8ab0827b9cf
Israel Batista <israel.silvino@gmail.com>
2025-10-19 21:26:55 -0300

    Make initial capacity more flexible and implement reversion for generic dynamic arrays

--- src/utils/da.h
+++ src/utils/da.h
@@ -1,11 +1,9 @@
 #ifndef DA_H
 #define DA_H
 
-/*
- * Macros for manipulating dynamic arrays, mostly yanked from nob.h (courtesy of Alexey Kutepov)
- */
 #include "allocator.h"
 #include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 #include <assert.h>
 
@@ -17,19 +15,24 @@ typedef struct {
     size_t item_size;
     size_t count;
     size_t capacity;
+    size_t min_capacity;
     allocator_t *allocator;
     void *alloc_ctx;
 } array_info_t;
 
-void *da_reserve(void *array, array_info_t *info, size_t expected_capacity) {
+static void *da_reserve(void *array, array_info_t *info, size_t expected_capacity) {
     assert(info->item_size != 0 && "You forgot to set the size");
     void *result = array;
 
     if ((expected_capacity) > info->capacity) {
         size_t old_size = info->count * info->item_size;
 
+        if (info->min_capacity == 0) {
+            info->min_capacity = DA_INIT_CAP;
+        }
+
         if (info->capacity == 0) {
-            info->capacity = DA_INIT_CAP;
+            info->capacity = info->min_capacity;
         }
 
         while ((expected_capacity) > info->capacity) {
@@ -43,7 +46,7 @@ void *da_reserve(void *array, array_info_t *info, size_t expected_capacity) {
      return result;
 }
 
-void *da_append(void *array, array_info_t *info, const void *item) {
+static void *da_append(void *array, array_info_t *info, const void *item) {
     void *result = array;
 
     size_t offset = info->count * info->item_size;
@@ -57,8 +60,26 @@ void *da_append(void *array, array_info_t *info, const void *item) {
     return result;
 }
 
+static void da_reverse(void *array, const array_info_t *info) {
+    uint8_t temp[info->item_size];
 
-void da_free(void *array, array_info_t *info) {
+    uint8_t *start = (uint8_t*) array;
+    uint8_t *end   = start + (info->count - 1) * info->item_size;
+
+    for (size_t i = 0; i < info->count / 2; ++i) {
+        size_t offset = i * info->item_size;
+        // memcpy(temp          , start + offset, info->item_size);
+        // memcpy(start + offset, end - offset  , info->item_size);
+        // memcpy(end - offset  , temp          , info->item_size);
+        for (size_t j = 0; j < info->item_size; ++j) {
+            temp[j] = start[offset + j];
+            start[offset +  j] = *(end - offset + j);
+            *(end - offset + j) = temp[j];
+        }
+    }
+}
+
+static void da_free(void *array, array_info_t *info) {
     info->allocator->free(info->alloc_ctx, array, info->capacity * info->item_size);
 }
 
