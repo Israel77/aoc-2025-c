52bb0e843d659c78175bed44226731d7dc02a9df
Israel Batista <israel.silvino@gmail.com>
2025-11-08 14:12:54 -0300

    update template

--- src/solutions/template/part1.c
+++ src/solutions/template/part1.c
@@ -1,7 +1,7 @@
 #include "prelude.h"
 #define PART_1_IMPL
 
-#define P1_THREADS 1
+#define P1_THREADS 2
 
 /* Shared data between threads */
 struct p1_data {
@@ -16,14 +16,16 @@ void *p1_solve(void *arg) {
     struct part_context *ctx = arg;
 
     /* IO and synchronization */
-    pthread_barrier_t barrier = ctx->common->barrier;
     string_t *input = ctx->common->input;
-    string_t output = ctx->common->output;
     size_t thread_count = ctx->common->thread_count;
     size_t thread_idx   = ctx->thread_idx;
 
-    printf("Not implemented yet!");
+    UNUSED(p1);
+    UNUSED(input);
+    UNUSED(thread_idx);
+    UNUSED(thread_count);
+
+    ctx->common->output = string_from_cstr("Not implemented yet!");
 
     return NULL;
 }
-
--- src/solutions/template/part2.c
+++ src/solutions/template/part2.c
@@ -16,13 +16,16 @@ void *p2_solve(void *arg) {
     struct part_context *ctx = arg;
 
     /* IO and synchronization */
-    pthread_barrier_t barrier = ctx->common->barrier;
     string_t *input = ctx->common->input;
-    string_t output = ctx->common->output;
     size_t thread_count = ctx->common->thread_count;
     size_t thread_idx   = ctx->thread_idx;
 
-    printf("Not implemented yet!");
+    UNUSED(p2);
+    UNUSED(input);
+    UNUSED(thread_idx);
+    UNUSED(thread_count);
+
+    ctx->common->output = string_from_cstr("Not implemented yet!");
 
     return NULL;
 }
